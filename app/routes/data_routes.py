de2b912338885c93052e6821e03ddb91b828bf5b
Miguecapi <zamiguelangel.zam@gmail.com>
2025-05-29 12:05:45 -0500

    token commit

--- app/routes/data_routes.py
+++ app/routes/data_routes.py
@@ -9,32 +9,19 @@ router = APIRouter()
 @router.post("/data_send")
 async def receive_data(
     data: useData,
-    authorization: str = Header(default="")
+    authorization: str = Header(default=None)
 ):
     json_data = data.dict()
     print("Datos recibidos:\n" + json.dumps(json_data, indent=4))
     print(f"Authorization header: {authorization}")
 
-    bearer_token = f"Bearer {authorization}" if authorization else "Bearer "
-
-    headers = {
-        "Authorization": bearer_token
-    }
-
     try:
         async with httpx.AsyncClient() as client:
-            response = await client.post(settings.FORWARD_URL, json=json_data, headers=headers)
-            try:
-                forwarded_response = response.json()
-            except ValueError:
-                forwarded_response = response.text
-
+            response = await client.post(settings.FORWARD_URL, json=json_data)
         return {
-            "message": "Datos reenviados correctamente",
-            "forwarded_response": forwarded_response,
-            "status_code": response.status_code
+            "message": "Datos de la encuesta recibidos y reenviados correctamente",
+            "forwarded_response": response.json()
         }
-
     except httpx.RequestError as exc:
         return {
             "message": "Error al reenviar los datos",
